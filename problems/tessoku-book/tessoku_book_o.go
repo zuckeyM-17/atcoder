897c41d259c143871c523bc0cd2baf0f09a111f9
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-05-06 00:43:23 +0900

    init A15

---
+++ problems/tessoku-book/tessoku_book_o.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var n, k int
+	fmt.Fscan(in, &n, &k)
+
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &a[i])
+	}
+
+	fmt.Fprintln(out, "No")
+}
