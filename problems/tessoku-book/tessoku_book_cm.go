989659f95d7ac2def5a4f72d268124d59d572258
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-05-06 00:30:40 +0900

    鉄則本B14

--- problems/tessoku-book/tessoku_book_cm.go
+++ problems/tessoku-book/tessoku_book_cm.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 func main() {
@@ -19,5 +20,44 @@ func main() {
 		fmt.Fscan(in, &a[i])
 	}
 
+	p := enumerate(a[:n/2])
+	q := enumerate(a[n/2:])
+
+	sort.Ints(q)
+
+	for _, v := range p {
+		if k-v < 0 {
+			continue
+		}
+		l, r := 0, len(q)-1
+		for l <= r {
+			m := (l + r) / 2
+			if q[m] == k-v {
+				fmt.Fprintln(out, "Yes")
+				return
+			}
+			if q[m] < k-v {
+				l = m + 1
+			} else {
+				r = m - 1
+			}
+		}
+	}
+
 	fmt.Fprintln(out, "No")
+}
+
+func enumerate(a []int) []int {
+	var sumList []int
+	for i := 0; i < 1<<len(a); i++ {
+		sum := 0
+		for j := 0; j < len(a); j++ {
+			w := 1 << j
+			if (i/w)%2 == 1 {
+				sum += a[j]
+			}
+		}
+		sumList = append(sumList, sum)
+	}
+	return sumList
 }
