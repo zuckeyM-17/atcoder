e0719be1e8dddb724356daf529a9bfb4a96eff11
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-05-06 01:30:50 +0900

    A15

--- problems/tessoku-book/tessoku_book_o.go
+++ problems/tessoku-book/tessoku_book_o.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 func main() {
@@ -11,13 +12,48 @@ func main() {
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 
-	var n, k int
-	fmt.Fscan(in, &n, &k)
+	var n int
+	fmt.Fscan(in, &n)
 
 	a := make([]int, n)
 	for i := 0; i < n; i++ {
 		fmt.Fscan(in, &a[i])
 	}
 
-	fmt.Fprintln(out, "No")
+	b := make([]int, n)
+	copy(b, a)
+
+	sort.Ints(b)
+
+	m := make(map[int]bool)
+	uniq := []int{}
+	for _, v := range b {
+		if !m[v] {
+			m[v] = true
+			uniq = append(uniq, v)
+		}
+	}
+
+	x := []int{}
+	for _, v := range a {
+		l, r := 0, len(uniq)
+		for l < r {
+			mid := (l + r) / 2
+			if uniq[mid] < v {
+				l = mid + 1
+			} else {
+				r = mid
+			}
+		}
+		x = append(x, l+1)
+	}
+
+	for i, v := range x {
+		fmt.Fprint(out, v)
+		if i < len(x)-1 {
+			fmt.Fprint(out, " ")
+		} else {
+			fmt.Fprintln(out, "")
+		}
+	}
 }
