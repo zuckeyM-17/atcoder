2a2497a064ba0c4bdef66d2e32b079965c11e7ca
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-05-05 23:49:08 +0900

    鉄則本A14

--- problems/tessoku-book/tessoku_book_n.go
+++ problems/tessoku-book/tessoku_book_n.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 func main() {
@@ -11,8 +12,52 @@ func main() {
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 
-	var n int
-	fmt.Fscan(in, &n)
+	var n, k int
+	fmt.Fscan(in, &n, &k)
 
-	fmt.Fprintln(out)
+	a, b, c, d := make([]int, n), make([]int, n), make([]int, n), make([]int, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &a[i])
+	}
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &b[i])
+	}
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &c[i])
+	}
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &d[i])
+	}
+
+	p, q := []int{}, []int{}
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			p = append(p, a[i]+b[j])
+			q = append(q, c[i]+d[j])
+		}
+	}
+
+	sort.Ints(q)
+
+	for _, v := range p {
+		if k-v < 0 {
+			continue
+		}
+		l, r := 0, len(q)-1
+		for l < r {
+			m := (l + r) / 2
+			if q[m] == k-v {
+				fmt.Fprintln(out, "Yes")
+				return
+			}
+			if q[m] < k-v {
+				l = m + 1
+			} else {
+				r = m
+			}
+		}
+	}
+
+	fmt.Fprintln(out, "No")
 }
