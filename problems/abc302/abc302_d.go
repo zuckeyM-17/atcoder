7a5a3a110ec7ccb0bb6415f695a2da97df959692
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-05-20 23:40:20 +0900

    abc302 d

--- problems/abc302/abc302_d.go
+++ problems/abc302/abc302_d.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 func main() {
@@ -11,8 +12,42 @@ func main() {
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 
-	var n int
-	fmt.Fscan(in, &n)
+	var n, m, d int
+	fmt.Fscan(in, &n, &m, &d)
 
-	fmt.Fprintln(out)
+	a, b := make([]int, n), make([]int, m)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &a[i])
+	}
+	for i := 0; i < m; i++ {
+		fmt.Fscan(in, &b[i])
+	}
+
+	sort.Ints(a)
+	sort.Ints(b)
+
+	for {
+		x, y := len(a), len(b)
+		if x == 0 || y == 0 {
+			fmt.Fprintln(out, -1)
+			break
+		}
+		if abs(a[x-1]-b[y-1]) <= d {
+			fmt.Fprintln(out, a[x-1]+b[y-1])
+			break
+		}
+
+		if a[x-1] > b[y-1] {
+			a = a[:x-1]
+		} else {
+			b = b[:y-1]
+		}
+	}
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
