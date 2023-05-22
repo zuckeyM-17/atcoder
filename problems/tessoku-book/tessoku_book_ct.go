645284b7f869fa805ca1615f0784690e4d3cd39c
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-05-23 00:06:46 +0900

    b21

--- problems/tessoku-book/tessoku_book_ct.go
+++ problems/tessoku-book/tessoku_book_ct.go
@@ -19,5 +19,38 @@ func main() {
 
 	s := []rune(str)
 
-	fmt.Fprintln(out, s)
+	dp := [1009][1009]int{}
+
+	for i := 0; i < n; i++ {
+		dp[i][i] = 1
+	}
+
+	for i := 0; i < n-1; i++ {
+		if s[i] == s[i+1] {
+			dp[i][i+1] = 2
+		} else {
+			dp[i][i+1] = 1
+		}
+	}
+
+	for length := 2; length <= n-1; length++ {
+		for l := 0; l < n-length; l++ {
+			r := l + length
+
+			if s[l] == s[r] {
+				dp[l][r] = max(max(dp[l][r-1], dp[l+1][r]), dp[l+1][r-1]+2)
+			} else {
+				dp[l][r] = max(dp[l][r-1], dp[l+1][r])
+			}
+		}
+	}
+
+	fmt.Fprintln(out, dp[0][n-1])
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
