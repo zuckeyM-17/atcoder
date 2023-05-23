507882a87333accb77a98e5e202d05d373ea588a
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-05-23 22:55:20 +0900

    fix a22

--- problems/tessoku-book/tessoku_book_v.go
+++ problems/tessoku-book/tessoku_book_v.go
@@ -26,6 +26,9 @@ func main() {
 	dp := make([]int, n+1)
 
 	dp[1] = 0
+	for i := 2; i <= n; i++ {
+		dp[i] = -1000000
+	}
 	for i := 1; i <= n-1; i++ {
 		dp[a[i]] = max(dp[a[i]], dp[i]+100)
 		dp[b[i]] = max(dp[b[i]], dp[i]+150)
