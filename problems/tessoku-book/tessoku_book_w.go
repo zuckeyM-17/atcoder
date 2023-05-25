0bf0dc072dd1b2cf214a45ea841692241978b790
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-05-26 00:32:25 +0900

    a23

--- problems/tessoku-book/tessoku_book_w.go
+++ problems/tessoku-book/tessoku_book_w.go
@@ -25,42 +25,30 @@ func main() {
 		}
 	}
 
-	dp := make([][]int, m)
+	b := make([]int, m)
 	for i := 0; i < m; i++ {
-		dp[i] = make([]int, 1<<n)
-		for j := 0; j < 1<<n; j++ {
-			dp[i][j] = 1 << 60
+		for j := 0; j < n; j++ {
+			b[i] += a[i][j] << j
 		}
 	}
 
-	dp[0][0] = 0
-	for i := 1; i < m; i++ {
+	dp := make([]int, 1<<n)
+	for i := 0; i < 1<<n; i++ {
+		dp[i] = 1 << 60
+	}
+
+	dp[0] = 0
+	for i := 0; i < m; i++ {
 		for j := 0; j < 1<<n; j++ {
-			already := make([]int, n)
-			for k := 0; k < n; k++ {
-				if j&1<<k != 0 {
-					already[k] = 1
-				}
-
-				v := 0
-				for k := 0; k < n; k++ {
-					if already[k] == 1 || a[i][k] == 1 {
-						v += 1 << k
-					}
-				}
-
-				dp[i][j] = min(dp[i][j], dp[i-1][j])
-				dp[i][v] = min(dp[i][v], dp[i-1][j]+1)
-			}
+			dp[j|b[i]] = min(dp[j|b[i]], dp[j]+1)
 		}
 	}
 
-	if dp[m-1][(1<<n)-1] == 1<<60 {
+	if dp[(1<<n)-1] == 1<<60 {
 		fmt.Fprintln(out, -1)
-		return
+	} else {
+		fmt.Fprintln(out, dp[(1<<n)-1])
 	}
-
-	fmt.Fprintln(out, dp[m-1][(1<<n)-1])
 }
 
 func min(a, b int) int {
