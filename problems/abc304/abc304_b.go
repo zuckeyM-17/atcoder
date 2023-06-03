93cde02cd97ca1e98cba129cbe0b45941255fd1d
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-06-03 21:11:14 +0900

    abc b

--- problems/abc304/abc304_b.go
+++ problems/abc304/abc304_b.go
@@ -14,5 +14,19 @@ func main() {
 	var n int
 	fmt.Fscan(in, &n)
 
-	fmt.Fprintln(out)
+	if 1000 > n {
+		fmt.Fprintln(out, n)
+	} else if 10000 > n {
+		fmt.Fprintln(out, n/10*10)
+	} else if 100000 > n {
+		fmt.Fprintln(out, n/100*100)
+	} else if 1000000 > n {
+		fmt.Fprintln(out, n/1000*1000)
+	} else if 10000000 > n {
+		fmt.Fprintln(out, n/10000*10000)
+	} else if 100000000 > n {
+		fmt.Fprintln(out, n/100000*100000)
+	} else {
+		fmt.Fprintln(out, n/1000000*1000000)
+	}
 }
