ddd063db9f1a13417d61f7349d576d23fa2b6fa0
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-06-03 21:07:24 +0900

    abc304 a

--- problems/abc304/abc304_a.go
+++ problems/abc304/abc304_a.go
@@ -14,5 +14,22 @@ func main() {
 	var n int
 	fmt.Fscan(in, &n)
 
-	fmt.Fprintln(out)
+	s, a := make([]string, n), make([]int, n)
+
+	min, first := 1000000009, -1
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &s[i], &a[i])
+		if a[i] < min {
+			min = a[i]
+			first = i
+		}
+	}
+
+	t := []string{}
+	t = append(t, s[first:]...)
+	t = append(t, s[:first]...)
+
+	for _, v := range t {
+		fmt.Fprintln(out, v)
+	}
 }
