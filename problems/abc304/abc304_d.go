be9b0cd6d8a7608dddbd88f8be70078efa72b9b6
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-06-03 23:44:01 +0900

    abc304 d

--- problems/abc304/abc304_d.go
+++ problems/abc304/abc304_d.go
@@ -3,16 +3,72 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"sort"
 )
+
+type tuple struct {
+	x, y int
+}
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 
-	var n int
+	var w, h, n int
+	fmt.Fscan(in, &w, &h)
 	fmt.Fscan(in, &n)
 
-	fmt.Fprintln(out)
+	p, q := make([]int, n), make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &p[i], &q[i])
+	}
+
+	var a, b int
+
+	fmt.Fscan(in, &a)
+	as := make([]int, a+2)
+	for i := 1; i <= a; i++ {
+		fmt.Fscan(in, &as[i])
+	}
+	as[a+1] = w
+
+	fmt.Fscan(in, &b)
+	bs := make([]int, b+2)
+	for i := 1; i <= b; i++ {
+		fmt.Fscan(in, &bs[i])
+	}
+	bs[b+1] = h
+
+	m := make(map[tuple]int)
+	for i := 0; i < n; i++ {
+		xIdx := sort.Search(len(as), func(k int) bool {
+			return p[i] < as[k]
+		})
+
+		yIdx := sort.Search(len(bs), func(k int) bool {
+			return q[i] < bs[k]
+		})
+		m[tuple{xIdx, yIdx}]++
+	}
+
+	var length, max int
+	min := math.MaxInt64
+
+	for _, v := range m {
+		length++
+		if max < v {
+			max = v
+		}
+		if v < min {
+			min = v
+		}
+	}
+	if length < (a+1)*(b+1) {
+		min = 0
+	}
+
+	fmt.Fprintln(out, min, max)
 }
