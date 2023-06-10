4afc902bc5ca58f434c1abcb8827d23eb4f0a83c
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-06-10 21:08:00 +0900

    abc30b

--- problems/abc305/abc305_b.go
+++ problems/abc305/abc305_b.go
@@ -11,8 +11,26 @@ func main() {
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 
-	var n int
-	fmt.Fscan(in, &n)
+	var p, q string
+	fmt.Fscan(in, &p, &q)
 
-	fmt.Fprintln(out)
+	m := map[string]int{
+		"A": 0,
+		"B": 3,
+		"C": 4,
+		"D": 8,
+		"E": 9,
+		"F": 14,
+		"G": 23,
+	}
+
+	x := abs(m[p] - m[q])
+	fmt.Fprintln(out, x)
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
