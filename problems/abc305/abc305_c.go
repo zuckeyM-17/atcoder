0a4d335653062624ca34bd9a49954e75f1d7bfd3
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-06-10 21:39:00 +0900

    abc305 c

--- problems/abc305/abc305_c.go
+++ problems/abc305/abc305_c.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -11,8 +12,40 @@ func main() {
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 
-	var n int
-	fmt.Fscan(in, &n)
+	var h, w int
+	fmt.Fscan(in, &h, &w)
 
-	fmt.Fprintln(out)
+	noneStrRow, noneStrCol := strings.Repeat(".", w), strings.Repeat(".", h)
+	cookieRows, cookieColumns := []int{}, []int{}
+	s := make([][]string, h+1)
+	for i := 1; i <= h; i++ {
+		s[i] = make([]string, w+1)
+		var str string
+		fmt.Fscan(in, &str)
+
+		if str != noneStrRow {
+			cookieRows = append(cookieRows, i)
+		}
+		for j := 1; j <= w; j++ {
+			s[i][j] = string(str[j-1])
+		}
+	}
+
+	for j := 1; j <= w; j++ {
+		str := ""
+		for i := 1; i <= h; i++ {
+			str += s[i][j]
+		}
+		if str != noneStrCol {
+			cookieColumns = append(cookieColumns, j)
+		}
+	}
+
+	for i := cookieRows[0]; i <= cookieRows[len(cookieRows)-1]; i++ {
+		for j := cookieColumns[0]; j <= cookieColumns[len(cookieColumns)-1]; j++ {
+			if s[i][j] == "." {
+				fmt.Fprintln(out, i, j)
+			}
+		}
+	}
 }
