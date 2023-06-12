a8a03ba84bd3beacc14af07f41d6c16f86f7688d
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-06-12 22:38:59 +0900

    b30

---
+++ problems/tessoku-book/tessoku_book_dc.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+	m := 1000000007
+	var h, w int
+	fmt.Fscan(in, &h, &w)
+
+	a, b := 1, 1
+
+	for i := w; i <= h+w-2; i++ {
+		a *= i
+		a %= m
+	}
+
+	for i := 1; i <= h-1; i++ {
+		b *= i
+		b %= m
+	}
+
+	fmt.Fprintln(out, a*power(b, m-2, m)%m)
+}
+
+func power(a, b, m int) int {
+	p, ans := a, 1
+	for i := 0; i < 30; i++ {
+		w := 1 << i
+		if (b/w)%2 == 1 {
+			ans = (ans * p) % m
+		}
+		p = (p * p) % m
+	}
+	return ans
+}
