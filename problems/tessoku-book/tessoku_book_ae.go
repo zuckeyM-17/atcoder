5aa8a6a4cf72b76c116f0a1e6575bb1bc5373f61
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-06-13 22:09:42 +0900

    A31

---
+++ problems/tessoku-book/tessoku_book_ae.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var n int
+	fmt.Fscan(in, &n)
+
+	ans := n/3 + n/5 - n/15
+
+	fmt.Fprintln(out, ans)
+}
