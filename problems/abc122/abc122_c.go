4fc8b7ec551c9d40c12c5c7c40397fe65e8bf8c5
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-07-18 03:16:52 +0900

    abc122 c

--- problems/abc122/abc122_c.go
+++ problems/abc122/abc122_c.go
@@ -11,8 +11,30 @@ func main() {
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 
-	var n int
-	fmt.Fscan(in, &n)
+	var n, q int
+	fmt.Fscan(in, &n, &q)
 
-	fmt.Fprintln(out)
+	var s string
+	fmt.Fscan(in, &s)
+
+	sum := make([]int, n+1)
+
+	for i := 0; i < n; i++ {
+		if i == 0 {
+			continue
+		}
+
+		if s[i-1] == 'A' && s[i] == 'C' {
+			sum[i] = sum[i-1] + 1
+		} else {
+			sum[i] = sum[i-1]
+		}
+	}
+
+	for i := 0; i < q; i++ {
+		var l, r int
+		fmt.Fscan(in, &l, &r)
+
+		fmt.Fprintln(out, sum[r-1]-sum[l-1])
+	}
 }
