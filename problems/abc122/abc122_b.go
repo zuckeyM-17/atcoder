f993186f5e742c230f5c03467d7d440a62b88513
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-07-18 03:04:02 +0900

    add abc122b

--- problems/abc122/abc122_b.go
+++ problems/abc122/abc122_b.go
@@ -11,8 +11,27 @@ func main() {
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 
-	var n int
-	fmt.Fscan(in, &n)
+	var s string
+	fmt.Fscan(in, &s)
 
-	fmt.Fprintln(out)
+	var max int
+
+	for i := 0; i < len(s); i++ {
+		for j := i; j < len(s); j++ {
+			if isACGT(s[i:j+1]) && max < j-i+1 {
+				max = j - i + 1
+			}
+		}
+	}
+
+	fmt.Fprintln(out, max)
+}
+
+func isACGT(s string) bool {
+	for _, r := range s {
+		if r != 'A' && r != 'C' && r != 'G' && r != 'T' {
+			return false
+		}
+	}
+	return true
 }
