b9a4666e893fccf944ef86d0d7ebb0960a8b6707
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-09-02 21:22:24 +0900

    abc318

--- problems/abc318/abc318_b.go
+++ problems/abc318/abc318_b.go
@@ -14,5 +14,31 @@ func main() {
 	var n int
 	fmt.Fscan(in, &n)
 
-	fmt.Fprintln(out)
+	var a, b, c, d int
+
+	s := make([][]bool, 101)
+	for i := range s {
+		s[i] = make([]bool, 101)
+	}
+
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &a, &b, &c, &d)
+		for j := a + 1; j <= b; j++ {
+			for k := c + 1; k <= d; k++ {
+				s[j][k] = true
+			}
+		}
+	}
+
+	ans := 0
+
+	for r := range s {
+		for i := range s[r] {
+			if s[r][i] {
+				ans += 1
+			}
+		}
+	}
+
+	fmt.Fprintln(out, ans)
 }
