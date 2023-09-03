2ed785fbd7d73bfae1a372727f8b95bb4135a44a
zuckeyM-17 <kazuki.matsumura@studist.jp>
2023-09-04 04:01:28 +0900

    wip

--- problems/abc318/abc318_c.go
+++ problems/abc318/abc318_c.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 func main() {
@@ -11,8 +12,54 @@ func main() {
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 
-	var n int
-	fmt.Fscan(in, &n)
+	var n, d, p int
+	fmt.Fscan(in, &n, &d, &p)
 
-	fmt.Fprintln(out)
+	f := make([]int, n)
+
+	passFee := p / d
+
+	type tuple struct {
+		date, fee int
+	}
+
+	overDate := []tuple{}
+	fee := 0
+
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &f[i])
+		if f[i] > passFee {
+			overDate = append(overDate, tuple{date: i, fee: f[i]})
+		} else {
+			fee += f[i]
+		}
+	}
+
+	if len(overDate) == 0 {
+		ans := 0
+		for i := range f {
+			ans += f[i]
+		}
+		fmt.Fprintln(out, ans)
+		return
+	}
+
+	sort.Slice(overDate, func(i, j int) bool {
+		return overDate[i].fee > overDate[j].fee
+	})
+
+	passCount := len(overDate) / d
+
+	rest := overDate[passCount*d:]
+	restFee := 0
+	for _, r := range rest {
+		restFee += r.fee
+	}
+	if restFee > p {
+		passCount++
+	} else {
+		fee += restFee
+	}
+
+	fmt.Fprintln(out, fee+passCount*p)
 }
--- problems/abc318/abc318_d.go
+++ problems/abc318/abc318_d.go
@@ -3,16 +3,33 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"image/jpeg"
 	"os"
 )
+
+type Edge struct {
+	i, j, w int
+}
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 
-	var n int
+	var n, w int
 	fmt.Fscan(in, &n)
+
+	edges := make([][]Edge, n-1)
+	for i := range edges {
+		edges[i] = make([]edge, n-1-i)
+		for j := range edges[i] {
+			fmt.Fscan(in, &w)
+			edges[i][j] = Edge{i: i, j: j + i + 1, w: w}
+		}
+	}
+
+
+	for 
 
 	fmt.Fprintln(out)
 }
